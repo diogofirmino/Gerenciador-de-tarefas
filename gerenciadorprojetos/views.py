de7e412dc1d5e5cc146a0586d9b520b11016e94e
ITCURSOS <aluno@ubuntu.(none)>
2011-11-01 16:30:18 -0700

    desafio 4 incompleto

--- gerenciadorprojetos/views.py
+++ gerenciadorprojetos/views.py
@@ -1,10 +1,12 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from gerenciadorprojetos.models import Project, ProjectForm
 from django.template import Context, loader
+from django.shortcuts import render_to_response, get_object_or_404
 
 
-def index(request):
+
+def project_index(request):
 	project_list = Project.objects.all()
 	t = loader.get_template('index.html')
 	c = Context({
@@ -13,15 +15,23 @@ def index(request):
     	
 	return HttpResponse(t.render(c))
 
-def create(request):
-	if request.POST:
-		formset = ProjectForm(request.POST)
-        	if formset.is_valid():
-            		formset.save()
-		else:
-			pass
+def project_new(request):
+	project_form = ProjectForm()
+	t = loader.get_template('new.html')
+	c = Context({'project_form': project_form,})
+	return HttpResponse(t.render(c))
+
+def project_create(request):
+	formset = ProjectForm(request.POST)
+       	if formset.is_valid():
+		formset.save()
+		return HttpResponseRedirect('/projects/')
 	else:
-		project_form = ProjectForm()
-		t = loader.get_template('create.html')
-		c = Context({'project_form': project_form,})
-		return HttpResponse(t.render(c))
+		pass
+
+def project_edit(request, project_id):
+	project = get_object_or_404(Project, pk=project_id)
+	p = TaskForm(instance=project)
+	t = 'projects/edit.html'
+	dic = {'ProjectForm':p,'id':project_id}
+	return render_to_response(t,dic)
--- urls.py
+++ urls.py
@@ -15,6 +15,8 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     
-    url(r'^projects/$', 'dev01.gerenciadorprojetos.views.index'),    
-    url(r'^projects/create', 'dev01.gerenciadorprojetos.views.create'),
+    url(r'^projects/$', 'dev01.gerenciadorprojetos.views.project_index'),    
+    url(r'^projects/create', 'dev01.gerenciadorprojetos.views.project_create'),
+    url(r'^projects/new', 'dev01.gerenciadorprojetos.views.project_new'),
+    url(r'^/project/(?P\d+)/edit$', 'dev01.gerenciadorprojetos.views.project_edit'),
 )
